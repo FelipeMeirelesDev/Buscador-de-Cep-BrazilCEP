1fa609841737f9634407d219096b4d0b93da747b
Felipe Meireles <145486045+FelipeMeirelesDev@users.noreply.github.com>
2024-08-02 12:18:58 -0300

    Adicionando o código do projeto

---
+++ Buscador de CEP/Buscador-Cep(Brazilcep).py
@@ -0,0 +1,14 @@
+#importando a biblioteca
+import brazilcep
+
+#brazilcep.get_address_from_cep("[Digite o cep]").
+#aqui vamos atribuir as informações na variavel "endereco".
+#coloquei o cep da minha cidade como exemplo.
+endereco = brazilcep.get_address_from_cep("70070600")
+
+#colando especificamente as informações cidade e estado nas respectivas variaveis.
+cidade = endereco.get("city")
+estado = endereco.get("uf")
+
+#Mostrando o resultado.
+print("A sua cidade é {} e seu estado é {}.".format(cidade, estado))
